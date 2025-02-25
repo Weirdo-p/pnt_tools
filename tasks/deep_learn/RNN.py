1a4be2036f8b5a70a8ba4685b9a131ad6070f47f
zhuoxu <zhuoxu@whu.edu.cn>
2025-02-25 17:25:44 +0800

    learn RNN

--- tasks/deep_learn/RNN.py
+++ tasks/deep_learn/RNN.py
@@ -18,11 +18,6 @@ def read_time_machine(): #@save
         lines = f.readlines()
     return [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in lines]
 
-lines = read_time_machine()
-logger.info(f'# text lines: {len(lines)}')
-logger.info(lines[0])
-logger.info(lines[10])
-
 def tokenize(lines, token='word'): #@save
     if token == 'word':
         return [line.split() for line in lines]
@@ -31,8 +26,6 @@ def tokenize(lines, token='word'): #@save
     else:
         logger.error(f'unknown token type {token}')
 
-tokens = tokenize(lines)
-logger.info(tokens[0:2])
 
 class Vocab: 
     def __init__(self, tokens=None, min_freq=0, reserved_tokens=None):
@@ -82,12 +75,216 @@ def count_corpus(tokens): #@save
 def load_corpus_time_machine(max_tokens=-1): #@save
     lines = read_time_machine()
     tokens = tokenize(lines, 'char')
-    logger.info(tokens)
+    # logger.info(tokens)
     vocab = Vocab(tokens)
     corpus = [vocab[token] for line in tokens for token in line]
     if max_tokens > 0:
         corpus = corpus[:max_tokens]
     return corpus, vocab
 
-corpus, vocab = load_corpus_time_machine()
-# len(corpus), len(vocab)
+import random
+
+def seq_data_iter_random(corpus, batch_size, num_steps):
+    corpus = corpus[random.randint(0, num_steps - 1):]
+    num_subseqs = (len(corpus) - 1) // num_steps
+    initial_indices = list(range(0, num_subseqs * num_steps, num_steps))
+    random.shuffle(initial_indices)
+    
+    def data(pos):
+        return corpus[pos:pos + num_steps]
+    
+    num_batches = num_subseqs // batch_size
+    for i in range(0, batch_size * num_batches, batch_size):
+        initial_indices_per_batch = initial_indices[i:i + batch_size]
+        X = [data(j) for j in initial_indices_per_batch]
+        Y = [data(j + 1) for j in initial_indices_per_batch]
+        yield torch.tensor(X), torch.tensor(Y)
+
+def seq_data_iter_sequential(corpus, batch_size, num_steps):
+    offset = random.randint(0, num_steps)
+    num_tokens = ((len(corpus) - offset - 1) // batch_size) * batch_size
+    Xs = torch.tensor(corpus[offset:offset + num_tokens])
+    Ys = torch.tensor(corpus[offset + 1:offset + 1 + num_tokens])
+    Xs, Ys = Xs.reshape(batch_size, -1), Ys.reshape(batch_size, -1)
+    num_batches = Xs.shape[1] // num_steps
+    for i in range(0, num_steps * num_batches, num_steps):
+        X = Xs[:, i:i + num_steps]
+        Y = Ys[:, i:i + num_steps]
+        yield X, Y
+
+
+class SeqDataLoader:
+    def __init__(self, batch_size, num_steps, use_random_iter, max_tokens):
+        if use_random_iter:
+            self.data_iter_fn = seq_data_iter_random
+        else:
+            self.data_iter_fn = seq_data_iter_sequential
+        self.corpus, self.vocab = load_corpus_time_machine(max_tokens)
+        self.batch_size, self.num_steps = batch_size, num_steps
+    def __iter__(self):
+        return self.data_iter_fn(self.corpus, self.batch_size, self.num_steps)
+
+def load_data_time_machine(batch_size, num_steps, use_random_iter=False, max_tokens=10000):
+    data_iter = SeqDataLoader(batch_size, num_steps, use_random_iter, max_tokens)
+    return data_iter, data_iter.vocab
+
+import math, torch
+from torch.nn import functional as F
+from torch import nn
+
+
+def get_params(vocab_size, num_hiddens, device):
+    num_inputs = num_outputs = vocab_size
+
+    def normal(shape):
+        return torch.randn(size=shape, device=device) * 0.01
+    
+    W_xh = normal((num_inputs, num_hiddens))
+    W_hh = normal((num_hiddens, num_hiddens))
+    b_h = torch.zeros(num_hiddens, device=device)
+
+    W_hq = normal((num_hiddens, num_outputs))
+    b_q = torch.zeros(num_outputs, device=device)
+
+    params = [W_xh, W_hh, b_h, W_hq, b_q]
+    for param in params:
+        param.requires_grad_(True)
+    return params
+
+def init_rnn_state(batch_size, num_hiddens, device):
+    return (torch.zeros((batch_size, num_hiddens), device=device), )
+
+def rnn(inputs, state, params):
+    W_xh, W_hh, b_h, W_hq, b_q = params
+    H, = state  
+    outputs = []
+    for X in inputs:
+        H = torch.tanh(torch.mm(X, W_xh) + torch.mm(H, W_hh) + b_h)
+        Y = torch.mm(H, W_hq) + b_q
+        outputs.append(Y)
+    return torch.cat(outputs, dim=0), (H,)
+
+class RNNModelScratch:
+    def __init__(self, vocab_size, num_hiddens, device, get_params, init_state, forward_fn):
+        self.vocab_size, self.num_hiddens = vocab_size, num_hiddens
+        self.params = get_params(vocab_size, num_hiddens, device)
+        self.init_state, self.forward_fn = init_state, forward_fn
+
+    def __call__(self, X, state):
+        X = F.one_hot(X.T, self.vocab_size).type(torch.float32)
+        return self.forward_fn(X, state, self.params)
+    
+    def begin_state(self, batch_size, device):
+        return self.init_state(batch_size, self.num_hiddens, device)
+    
+
+def predict(prefix, num_preds, net, vocab, device):
+    state = net.begin_state(batch_size=1, device=device)
+    outputs = [vocab[prefix[0]]]
+    get_input = lambda: torch.tensor([outputs[-1]], device=device).reshape((1, 1))
+    for y in prefix[1:]:
+        _, state = net(get_input(), state)
+        outputs.append(vocab[y])
+    for _ in range(num_preds):
+        y, state = net(get_input(), state)
+        outputs.append(int(y.argmax(dim=1).reshape(1)))
+    return ''.join([vocab.idx_to_token[i] for i in outputs])
+
+# logger.warning(predict('time traveller', 10, net, vocab, d2l.try_gpu()))
+
+def grad_clipping(net, theta):
+    if isinstance(net, nn.Module):
+        params = [p for p in net.parameters() if p.requires_grad]
+    else:
+        params = net.params
+    norm = torch.sqrt(sum(torch.sum((p.grad ** 2)) for p in params))
+    if norm > theta:
+        for param in params:
+            param.grad[:] *= theta / norm
+    return norm
+
+def train_epoch(net, train_iter, loss, updater, device, use_random_iter):
+    state, timer = None, d2l.Timer()
+    metric = d2l.Accumulator(2)
+    for X, Y in train_iter:
+        if state is None or use_random_iter:
+            state = net.begin_state(batch_size=X.shape[0], device=device)
+        else:
+            if isinstance(net, nn.Module) and not isinstance(state, tuple):
+                state.detach_()
+            else:
+                for s in state:
+                    s.detach_()
+        y = Y.T.reshape(-1)
+        X, y = X.to(device), y.to(device)
+        y_hat, state = net(X, state)
+        l = loss(y_hat, y.long()).mean()
+        if isinstance(updater, torch.optim.Optimizer):
+            updater.zero_grad()
+            l.backward()
+            grad_clipping(net, 1)
+            updater.step()
+        else:
+            l.backward()
+            grad_clipping(net, 1)
+            updater(batch_size=1)
+        metric.add(l * y.numel(), y.numel())
+    return math.exp(metric[0] / metric[1]), metric[1] / timer.stop()
+
+def train(net, train_iter, vocab, lr, num_epochs, device, use_random_iter=False):
+    loss = nn.CrossEntropyLoss()
+
+    if isinstance(net, nn.Module):
+        updater = torch.optim.SGD(net.parameters(), lr)
+    else:
+        updater = lambda batch_size: d2l.sgd(net.params, lr, batch_size)
+    predict_next = lambda prefix: predict(prefix, 50, net, vocab, device)
+
+    for epoch in range(num_epochs):
+        ppl, speed = train_epoch(net, train_iter, loss, updater, device, use_random_iter)
+        if (epoch + 1) % 10 == 0:
+            logger.warning(f'Epoch {epoch + 1}, Perplexity: {ppl:.1f}, Speed: {speed:.1f} tokens/sec')
+            predict_next('time traveller')
+    logger.warning(f'Perplexity: {ppl:.1f}, Speed: {speed:.1f} tokens/sec')
+    logger.warning(predict_next('time traveller'))
+    logger.warning(predict_next('traveller'))
+
+
+if __name__ == '__main__':
+    lines = read_time_machine()
+    logger.info(f'# text lines: {len(lines)}')
+    logger.info(lines[0])
+    logger.info(lines[10])
+
+
+    corpus, vocab = load_corpus_time_machine()
+    # logger.info(corpus)
+    # len(corpus), len(vocab)
+    tokens = tokenize(lines)
+    logger.info(tokens[0:2])
+
+    import random
+
+    tokens = tokenize(read_time_machine())
+    corpus = [token for line in tokens for token in line]
+    vocab = Vocab(corpus)
+    logger.info(vocab.token_freqs[:10])
+    logger.info(vocab.token_freqs[-10:])
+    bigram_tokens = [pair for pair in zip(corpus[:-1], corpus[1:])]
+    bigram_vocab = Vocab(bigram_tokens)
+    logger.info(bigram_vocab.token_freqs[:10])
+    # logger.info(bigram_tokens[:10])
+
+    my_seq = list(range(35))
+    for X, Y in seq_data_iter_random(my_seq, batch_size=2, num_steps=5):
+        logger.info(f'X: {X}, Y: {Y}')
+    batch_size, num_steps = 32, 35
+    train_iter, vocab = load_data_time_machine(batch_size, num_steps)
+    # check
+    num_hiddens = 512
+    net = RNNModelScratch(len(vocab), num_hiddens, d2l.try_gpu(), get_params, init_rnn_state, rnn)
+    state = net.begin_state(X.shape[0], d2l.try_gpu())
+    Y, new_state = net(X.to(d2l.try_gpu()), state)
+    Y.shape, len(new_state), new_state[0].shape
+    num_epochs, lr = 500, 1
+    train(net, train_iter, vocab, lr, num_epochs, d2l.try_gpu())
