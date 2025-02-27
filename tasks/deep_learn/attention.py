d958d9b2d5a70037f42c0a081da83cbc1ebe610e
zhuoxu <zhuoxu@whu.edu.cn>
2025-02-27 20:09:34 +0800

    learn attention

--- tasks/deep_learn/attention.py
+++ tasks/deep_learn/attention.py
@@ -5,6 +5,7 @@ import collections, re, os, sys
 sys.path.insert(
     0, os.path.dirname(os.path.abspath(__file__)) + "/../../")
 from tasks.deep_learn.RNN import load_data_time_machine, train
+from tasks.deep_learn.translator import sequence_mask, Decoder, Seq2SeqEncoder, load_data_nmt, train_seq2seq, EncoderDecoder
 from utils.logger.logger import logger
 from tasks.deep_learn.try_gpu import *
 
@@ -25,7 +26,120 @@ class NWKernelRegression(nn.Module):
         # values的形状为(查询个数，“键－值”对个数)
         return torch.bmm(self.attention_weights.unsqueeze(1),
                          values.unsqueeze(-1)).reshape(-1)
+
+def masked_softmax(X, valid_lens):
+    """通过在最后一个轴上掩蔽元素来执行softmax操作"""
+    if valid_lens is None:
+        return nn.functional.softmax(X, dim=-1)
+    else:
+        shape = X.shape
+        if valid_lens.dim() == 1:
+            valid_lens = torch.repeat_interleave(valid_lens, shape[1])
+        else: 
+            valid_lens = valid_lens.reshape(-1)
+        # 最后一轴上被掩蔽的元素使用一个非常大的负值替换，从而其softmax输出为0
+        X = sequence_mask(X.reshape(-1, shape[-1]), valid_lens, value=-1e6)
+        return nn.functional.softmax(X.reshape(shape), dim=-1)
     
+class AdditiveAttention(nn.Module):
+    def __init__(self, key_size, query_size, num_hiddens, dropout, **kwargs):
+        super(AdditiveAttention, self).__init__(**kwargs)
+        self.W_k = nn.Linear(key_size, num_hiddens, bias=False)
+        self.W_q = nn.Linear(query_size, num_hiddens, bias=False)
+        self.w_v = nn.Linear(num_hiddens, 1, bias=False)
+        self.dropout = nn.Dropout(dropout)
+    
+    def forward(self, queries, keys, values, valid_lens):
+        queries, keys = self.W_q(queries), self.W_k(keys)
+
+        features = queries.unsqueeze(2) + keys.unsqueeze(1)
+        features = torch.tanh(features)
+        scores = self.w_v(features).squeeze(-1)
+        self.attention_weights = masked_softmax(scores, valid_lens)
+        return torch.bmm(self.dropout(self.attention_weights), values)
+
+import math
+class DotProductAttention(nn.Module):
+    def __init__(self, dropout, **kwargs):
+        super(DotProductAttention, self).__init__(**kwargs)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, queries, keys, values, valid_lens=None):
+        d = queries.shape[-1]
+        scores = torch.bmm(queries, keys.transpose(1, 2)) / math.sqrt(d)
+        self.attention_weights = masked_softmax(scores, valid_lens)
+        return torch.bmm(self.dropout(self.attention_weights), values)
+
+class AttentionDecoder(Decoder):
+    def __init__(self, **kwargs):
+        super(AttentionDecoder, self).__init__(**kwargs)
+
+    @property
+    def attention_weights(self):
+        raise NotImplementedError
+
+class Seq2SeqAttentionDecoder(AttentionDecoder):
+    def __init__(self, vocab_size, embed_size, num_hiddens, num_layers, dropout=0, **kwargs):
+        super(Seq2SeqAttentionDecoder, self).__init__(**kwargs)
+        self.attention = AdditiveAttention(num_hiddens, num_hiddens, num_hiddens, dropout)
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.rnn = nn.GRU(embed_size + num_hiddens, num_hiddens, num_layers, dropout=dropout)
+        self.dense = nn.Linear(num_hiddens, vocab_size)
+    
+    def init_state(self, enc_outputs, enc_valid_lens, *args):
+        outputs, hidden_state = enc_outputs
+        return (outputs.permute(1, 0, 2), hidden_state, enc_valid_lens)
+    
+    def forward(self, X, state):
+        enc_outputs, hidden_state, enc_valid_lens = state
+        X = self.embedding(X).permute(1, 0, 2)
+        outputs, self._attention_weights = [], []
+        for x in X:
+            query = torch.unsqueeze(hidden_state[-1], dim=1)
+            context = self.attention(query, enc_outputs, enc_outputs, enc_valid_lens)
+            x = torch.cat((context, torch.unsqueeze(x, dim=1)), dim=-1)
+            out, hidden_state = self.rnn(x.permute(1, 0, 2), hidden_state)
+            outputs.append(out)
+            self._attention_weights.append(self.attention.attention_weights)
+        outputs = self.dense(torch.cat(outputs, dim=0))
+        return outputs.permute(1, 0, 2), [enc_outputs, hidden_state, enc_valid_lens]
+    
+    @property
+    def attention_weights(self):
+        return self._attention_weights
+
+def transpose_qkv(X, num_heads):
+    X = X.reshape(X.shape[0], X.shape[1], num_heads, -1)
+    X = X.permute(0, 2, 1, 3)
+    return X.reshape(-1, X.shape[2], X.shape[3])
+
+def transpose_output(X, num_threads):
+    X = X.reshape(-1, num_threads, X.shape[1], X.shape[2])
+    X = X.permute(0, 2, 1, 3)
+    return X.reshape(X.shape[0], X.shape[1], -1)
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, key_size, query_size, value_size, num_hiddens, num_heads, dropout, bias=False, **kwargs):
+        super(MultiHeadAttention, self).__init__(**kwargs)
+        self.num_heads = num_heads
+        self.attention = DotProductAttention(dropout)
+        self.W_q = nn.Linear(query_size, num_hiddens, bias=bias)
+        self.W_k = nn.Linear(key_size, num_hiddens, bias=bias)
+        self.W_v = nn.Linear(value_size, num_hiddens, bias=bias)
+        self.W_o = nn.Linear(num_hiddens, num_hiddens, bias=bias)
+
+    def forward(self, queries, keys, values, valid_lens):
+        queries = transpose_qkv(self.W_q(queries), self.num_heads)
+        keys = transpose_qkv(self.W_k(keys), self.num_heads)
+        values = transpose_qkv(self.W_v(values), self.num_heads)
+        if valid_lens is not None:
+            valid_lens = torch.repeat_interleave(
+                valid_lens, repeats=self.num_heads, dim=0)
+        output = self.attention(queries, keys, values, valid_lens)
+        output_concat = transpose_output(output, self.num_heads)
+        return self.W_o(output_concat)
+    
+
 if __name__ == "__main__":
 
     n_train = 50
@@ -64,3 +178,42 @@ if __name__ == "__main__":
         l.sum().backward()
         trainer.step()
         logger.info(f'epoch {epoch + 1}, loss {float(l.sum()):.6f}')
+
+    logger.info(masked_softmax(torch.rand(2, 2, 4), torch.tensor([2, 3])))
+
+    queries, keys = torch.normal(0, 1, (2, 1, 20)), torch.ones((2, 10, 2))
+    values = torch.arange(40, dtype=torch.float32).reshape(1, 10, 4).repeat(2, 1, 1)
+    valid_lens = torch.tensor([2, 6])
+    logger.info(f"{queries.shape}, {keys.shape}, {values.shape}")
+    attention = AdditiveAttention(key_size=2, query_size=20, num_hiddens=8, dropout=0.1)
+    attention.eval()
+    logger.info(attention(queries, keys, values, valid_lens).shape)
+
+    queries = torch.normal(0, 1, (2, 1, 2))
+    attention = DotProductAttention(dropout=0.5)
+    attention.eval()
+    logger.info(attention(queries, keys, values, valid_lens))
+
+    encoder = Seq2SeqEncoder(vocab_size=10, embed_size=8, num_hiddens=16, num_layers=2)
+    encoder.eval()
+
+    decoder = Seq2SeqAttentionDecoder(vocab_size=10, embed_size=8, num_hiddens=16, num_layers=2)
+    decoder.eval()
+
+    X = torch.zeros((4, 7), dtype=torch.long)
+    state = decoder.init_state(encoder(X), None)
+    output, state = decoder(X, state)
+
+    logger.info(f"{output.shape}, {len(state)}, {state[0].shape}, {len(state[1])}, {state[1][0].shape}")
+
+
+    embed_size, num_hiddens, num_layers, dropout = 32, 32, 2, 0.1
+    batch_size, num_steps = 64, 10
+    lr, num_epochs, device = 0.005, 250, d2l.try_gpu()
+    train_iter, src_vocab, tgt_vocab = load_data_nmt(batch_size, num_steps)
+    encoder = Seq2SeqEncoder(
+        len(src_vocab), embed_size, num_hiddens, num_layers, dropout)
+    decoder = Seq2SeqAttentionDecoder(
+        len(tgt_vocab), embed_size, num_hiddens, num_layers, dropout)
+    net = EncoderDecoder(encoder, decoder)
+    train_seq2seq(net, train_iter, lr, num_epochs, tgt_vocab, device)
